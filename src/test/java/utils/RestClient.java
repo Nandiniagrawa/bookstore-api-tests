940e6b811464d4ecde6fe6c55c8b7b8a7aaaa383
Nandiniagrawa <129610536+Nandiniagrawa@users.noreply.github.com>
2025-06-08 15:07:22 +0530

    Update RestClient.java

--- src/test/java/utils/RestClient.java
+++ src/test/java/utils/RestClient.java
@@ -1,26 +1,54 @@
-package utils;
-
-import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
 
 public class RestClient {
     public static Response post(String endpoint, Object body) {
-        return given().contentType("application/json").body(body)
-                .when().post(endpoint);
+        return given()
+                .contentType("application/json")
+                .body(body)
+                .log().all()
+            .when()
+                .post(endpoint)
+            .then()
+                .log().all()
+                .extract()
+                .response();
     }
 
     public static Response get(String endpoint) {
-        return given().contentType("application/json")
-                .when().get(endpoint);
+        return given()
+                .contentType("application/json")
+                .log().all()
+            .when()
+                .get(endpoint)
+            .then()
+                .log().all()
+                .extract()
+                .response();
     }
 
     public static Response put(String endpoint, Object body) {
-        return given().contentType("application/json").body(body)
-                .when().put(endpoint);
+        return given()
+                .contentType("application/json")
+                .body(body)
+                .log().all()
+            .when()
+                .put(endpoint)
+            .then()
+                .log().all()
+                .extract()
+                .response();
     }
 
     public static Response delete(String endpoint) {
-        return given().contentType("application/json")
-                .when().delete(endpoint);
+        return given()
+                .contentType("application/json")
+                .log().all()
+            .when()
+                .delete(endpoint)
+            .then()
+                .log().all()
+                .extract()
+                .response();
     }
 }
